84b5e68e906e7d1ece2b9d492983c6fc57ea3f73
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-14 14:18:40 -0600

    We Created Gatsby Pages
    Working with changing the URL

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Commodo elit at
+        imperdiet dui accumsan sit amet nulla facilisi. Feugiat in fermentum
+        posuere urna nec. Suspendisse in est ante in nibh mauris cursus mattis
+        molestie. Varius morbi enim nunc faucibus a pellentesque sit amet. Quis
+        imperdiet massa tincidunt nunc pulvinar. Libero enim sed faucibus turpis
+        in eu mi bibendum. Suspendisse faucibus interdum posuere lorem ipsum
+        dolor. Curabitur gravida arcu ac tortor dignissim convallis aenean et.
+        Nunc sed id semper risus. Suscipit tellus mauris a diam maecenas sed
+        enim. Augue eget arcu dictum varius. Pulvinar pellentesque habitant
+        morbi tristique senectus. Sed enim ut sem viverra aliquet eget sit. Sed
+        id semper risus in hendrerit. Eget magna fermentum iaculis eu non.
+        <br />
+        Commodo viverra maecenas accumsan lacus vel facilisis volutpat est.
+        Morbi blandit cursus risus at ultrices mi tempus imperdiet. Neque
+        sodales ut etiam sit amet nisl purus. Non odio euismod lacinia at quis
+        risus. Diam in arcu cursus euismod quis viverra. Sed vulputate mi sit
+        amet mauris commodo quis. In aliquam sem fringilla ut morbi tincidunt
+        augue. Nulla at volutpat diam ut venenatis tellus.
+      </p>
+    </div>
+  )
+}
+export default AboutPage
---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <p>Something will show up here</p>
+    </div>
+  )
+}
+export default BlogPage
