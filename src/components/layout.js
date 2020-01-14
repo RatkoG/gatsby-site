0070514ec22ea71a6e15aaca4ed99e1d717caa97
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-14 16:47:44 -0600

    Just for Starting with wtyle

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import "../styles/index.css"
 const Layout = props => {
   return (
     <div>
