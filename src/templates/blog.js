a226cec4e114293aa16088a25bb1356f9a96dfe2
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-16 13:12:35 -0600

    Dynamic Pages from Contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,25 +2,31 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+//`
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedData(fromNow: true)
     }
   }
 `
+
 const Blog = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title} </h1>
+      <p> {props.data.contentfulBlogPost.publishedData}</p>
     </Layout>
   )
 }
