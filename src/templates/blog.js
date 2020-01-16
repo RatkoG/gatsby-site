79ffd8fc9a45315ad9c716e8cfff9cd07237a86c
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-16 13:37:08 -0600

    Rendering Body & Images

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 // export const query = graphql`
 //   query($slug: String!) {
@@ -18,15 +19,31 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedData(fromNow: true)
+      body {
+        json
+      }
     }
   }
 `
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img src={url} alt={alt} />
+      },
+    },
+  }
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title} </h1>
       <p> {props.data.contentfulBlogPost.publishedData}</p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
