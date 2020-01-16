2f6009441804d4d1c74003c93b58718091ee3640
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-16 14:28:26 -0600

    Dinamic site title

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
 
 const NotFoundPage = () => (
   <Layout>
+    <Head title="404" />
     <h1>NOT FOUND</h1>
     <p>
       <Link to="/">Head Home</Link>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
 const AboutPage = () => {
   return (
     <Layout>
+      <Head title="About" />
       <h1>About Me</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
