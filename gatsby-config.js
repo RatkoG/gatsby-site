4352298525229ac2eee55ff4ccb1c29bb9871bbc
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-01-27 08:13:19 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: "src",
+        name: `src`,
         path: `${__dirname}/src/`,
       },
     },
